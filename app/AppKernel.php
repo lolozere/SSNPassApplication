d0a07388b15e70794e61b8a659b296430d2b1d7a
Laurent Chedanne <laurent@chedanne.pro>
2013-07-11 22:40:03 +0200

    Ajout du vendor APYDataGridBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,10 +16,12 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+        	new APY\DataGridBundle\APYDataGridBundle(),
             new SSN\TherapassBundle\SSNTherapassBundle(),
             new Oxygen\FrameworkBundle\OxygenFrameworkBundle(),
             new Oxygen\WeezeventBundle\OxygenWeezeventBundle(),
             new Oxygen\PassbookBundle\OxygenPassbookBundle(),
+            new Oxygen\DatagridBundle\OxygenDatagridBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
