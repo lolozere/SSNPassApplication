e926f3aa1503c09d205e641e3058c662f03c09df
Laurent Chedanne <laurent@chedanne.pro>
2013-09-24 23:05:50 +0200

    Update approche salon pour clôture réas

---
+++ app/DoctrineMigrations/Version20130924223828.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20130924223828 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE ssn_therapass_event ADD bookingsClosed TINYINT(1) NOT NULL");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE ssn_therapass_event DROP bookingsClosed");
+    }
+}
