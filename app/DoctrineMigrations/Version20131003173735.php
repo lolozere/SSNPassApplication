d4936f75ecf0b3dee01fea0e253ebbe704f36d13
Laurent Chedanne <laurent@chedanne.pro>
2013-10-03 17:42:53 +0200

    Bug fix if no participants for ticket Get all participants of ticket Demande n° de téléphone Lister les évènements passés

---
+++ app/DoctrineMigrations/Version20131003173735.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20131003173735 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE ssn_therapass_booking_person ADD phone VARCHAR(30) DEFAULT NULL");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE ssn_therapass_booking_person DROP phone");
+        $this->addSql("ALTER TABLE ssn_therapass_event DROP bookingsClosed");
+    }
+}
